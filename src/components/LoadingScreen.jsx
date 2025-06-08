44453533d589102c9019ef56d42c8111d79093c8
Ayesha Quazi <aayeshaquazi04@gmail.com>
2025-06-08 09:59:05 +0530

    Loading Screen

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+export const LoadingScreen = () {
+    return (
+        <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+
+        <div>
+           
+        </div>
+        
+        
+        </div>
+    )
+}
